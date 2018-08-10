467f8df1b25f5601d10f724e8cb7f9b2abff0d99
ethan1022 <ethan1022@gmail.com>
2018-08-10 11:15:55 +0800

    Add wkWebView and try to download something

--- webViewTest/ViewController.swift
+++ webViewTest/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.webView.navigationDelegate = self
+        let url = URL(string: "http://www.lingala.net/zip4j/download.php")
+        let request = URLRequest(url: url!)
+        self.webView.load(request)
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if navigationAction.navigationType == WKNavigationType.linkActivated {
+            print("link")
+            decisionHandler(.allow) // allow or cancel action
+            return
+        }
+        print("no link")
+        decisionHandler(.allow)
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        // get header and print it
+        if let response = navigationResponse.response as? HTTPURLResponse {
+            let header = response.allHeaderFields
+            print(header)
+        }
+        decisionHandler(.allow)
     }
 
-
+ /*
+    More detail can see...
+     
+     https://www.jianshu.com/p/d29384454a9a
+     https://stackoverflow.com/questions/34547161/downloading-files-wkwebview-ios
+     
+ */
 }
 
